7cbcb30ad4b7eefb6cc497514aa0c28aa3794c6d
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-16 16:25:40 -0300

    feat: user can see register screen

---
+++ pages/register.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Header :isLogged="false" :isBorder="true" title="Sign In"/>
+        <UserForm :isRegister="true" title="Sign Up"/>
+    </div>
+</template>
+
+<script>
+import "../styles/colors.css"
+import "../styles/reset.css"
+
+export default {
+
+}
+</script>
