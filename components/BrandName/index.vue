f6a9378232f86b607202445679f7b9f7d6b111aa
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-16 16:20:17 -0300

    feat: logo can be used in multiples components

---
+++ components/BrandName/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="title">Bookue</h1>
+</template>
+
+<style>
+.title {
+    display: flex;
+    justify-content: center;
+    color: var(--primary-color);
+    font-size: 22px;
+    margin-top: 16px;
+}
+</style>
