dabe27f4a9e7127fbebcb6773057eff337e19199
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-16 16:25:10 -0300

    feat: user can see your books

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial/>
+    <div class="home">
+        <Header :isLogged="true" :isBorder="true" :isHome="true" title="Sign up"/>
+        <ul class="home__list">
+            <Book v-for="item in list" />
+        </ul>
+    </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import "../styles/colors.css"
+import "../styles/reset.css"
 
-export default Vue.extend({})
+export default {
+    data() {
+        return {
+            list: ["dkadksa", "dksakdsa", "kdkasdksa", "kdaskda", "kdskdsa"]
+        }
+    }    
+}
 </script>
+
+<style scoped>
+.home {
+
+}
+
+.home__list {
+    margin-top: 32px;
+}
+</style>
