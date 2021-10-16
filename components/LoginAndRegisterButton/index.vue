bd073eea29acd01065ca4bc843de620c74e35ee6
Alex Nicolas <alexoliveira604@gmail.com>
2021-10-16 16:23:44 -0300

    feat: same button can be used in login in register screen

---
+++ components/LoginAndRegisterButton/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <button :class="Btn">{{ title }}</button>    
+</template>
+
+<style>
+.aligns {
+    position: absolute;
+    top: 16px;
+    right: 16px
+}
+
+.btn {
+    font-size: 20px;
+    width: 125px;
+    height: 50px;
+    border-radius: 2.5px;
+    outline: none;
+}
+
+.default-btn {
+    background: var(--primary-color);
+    color: var(--white-color);
+    border: none;
+}
+
+.default-btn:hover {
+    background: none;
+    border: 2.5px solid var(--primary-color);
+    color: var(--primary-color);
+    transition: 0.2s;
+}
+
+.btn-with-border {
+    background: none;
+    border: 2.5px solid var(--primary-color);
+    color: var(--primary-color);
+}
+
+.btn-with-border:hover {
+    background: var(--primary-color);
+    color: var(--white-color);
+    transition: 0.2s;
+}
+</style>
+
+<script>
+
+export default {
+    props: ["title", "border", "right", "isHeader"],
+    data() {
+        return {
+            Btn: {
+                "aligns": this.isHeader,
+                "btn": true,
+                "default-btn": !this.border,
+                "btn-with-border": this.border,
+            }
+        }
+    },
+    mounted() {
+        console.log(this.title)
+    }
+}
+</script>
