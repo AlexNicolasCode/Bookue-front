5380f8568a6b8c8890a317312fd9027fd6493c9c
AlexNicolasCode <alexoliveira604@gmail.com>
2021-11-01 16:25:27 -0300

    feat: user can do login and create new account

--- src/components/UserForm/index.vue
+++ src/components/UserForm/index.vue
@@ -3,14 +3,14 @@
         <BrandName class="container__title" />
         <div v-if="isInvalidForm && isRegisterForm">This account already exist.</div>
         <div v-if="isInvalidForm && !isRegisterForm">Invalid login, check your email and password.</div>
-        <form class="form">
-            <input class="form__item" @change="user.name" v-if="isRegisterForm" name="name" placeholder="name" />
+        <form @submit="submitForm" class="form">
+            <input class="form__item" v-model="user.name" v-if="isRegisterForm" name="name" placeholder="name" />
             <label v-if="isNameAlert">Name invalid</label>
-            <input class="form__item" @change="user.email" name="email" type="email" placeholder="email" />
+            <input class="form__item" v-model="user.email" name="email" type="email" placeholder="email" />
             <label v-if="isEmailAlert">Email invalid</label>
-            <input class="form__item" @change="user.password" name="password" type="password" placeholder="password" />
+            <input class="form__item" v-model="user.password" name="password" type="password" placeholder="password" />
             <label v-if="isPasswordAlert">Password invalid. Please, set a password with more than 3 letters, numbers or simbols</label>
-            <LoginAndRegisterButton class="form__btn" :submitForm="submitForm" :border="false" :title="title" :isHeader="false"/>
+            <LoginAndRegisterButton class="form__btn" @click="submitForm" :border="false" :title="title" :isHeader="false"/>
         </form>
     </div>
 </template>
@@ -30,69 +30,48 @@ export default {
         BrandName
     },
     props: ["isRegisterForm", "title"],
-    apollo: {
-        // Simple query that will update the 'hello' vue property
-        hello: gql`{
-            loginUser(email: $email, password: $password) {
-                token
-            }
-        }`,
-    },
     data() {
         return {
             user: {
-                name: "",
-                email: "",
-                password: ""
+                name: null,
+                email: null,
+                password: null
             },
             isInvalidForm: false,
             isNameAlert: false,
             isNameAlert: false,
             isEmailAlert: false,
-            isPasswordAlert: false
+            isPasswordAlert: false,
+            isRegister: this.isRegisterForm,
         }
     },
     mounted() {
-        if (this.isLogin()) {
-            console.log(localStorage.getItem("user"))
-            router.push({ path: "/home" });
+        if (this.isLogged()) {
+            this.redirectToHome()  
         }
-        this.mutationTeste()
     },
     methods: {
-        mutationTeste: async () => {
-            await apolloClient.mutate({ mutation: gql`mutation ($password: String!, $email: String!, $name: String!) {
-                signUpUser(password: $password, email: $email, name: $name) {
-                    token
-                }}`,
-
-                variables: {
-                    name: this.user.name,
-                    email: this.user.email, 
-                    password: this.user.password,
-                },
-            }).then((res) => {
-                const token = res.data.signUpUser.token;
-                if (token) localStorage.setItem("user", token)
-            })
+        redirectToHome() {
+            router.push({ path: "/home" });
         },
-        isLogin: async () => {
-            return localStorage.getItem("user") ? true : false
+        isLogged() {
+            return localStorage.getItem("user")
         },
-        submitForm: () => {
-            if (isRegisterForm) {
+        submitForm(event) {
+            event.preventDefault();
+            if (this.isRegister) {
                 this.registerUser()
                 return
             }
 
             this.loginUser()
         },
-        loginUser: async () => {
-            if (!this.validateUserEmail() || !this.validateUserPassword()) {
+        async loginUser() {
+            if (this.isValidUserEmail() || this.isValidPassword()) {
                 return
             }
 
-            await apolloClient.query({ query: gql`($email: String!, $password: String!){
+            const user = await apolloClient.query({ query: gql`($email: String!, $password: String!){
                 loginUser(email: $email, password: $password) {
                     token
                 }}`, 
@@ -101,24 +80,23 @@ export default {
                     email: this.user.email, 
                     password: this.user.password,
                 },
-            }).then(async (res) => {
-                const token = res.data.signUpUser.token;
-
-                if (!token) {
-                    return false
-                }
-
-                await saveTokenInLocalStorage(token)
-                return true
             })
+           const token = user.data.loginUser.token;
 
+            if (!token) {
+                this.isInvalidForm = true
+            }
+
+            this.isInvalidForm = false
+            await this.saveTokenInLocalStorage(token)
+            this.redirectToHome()
         },
-        registerUser: async () => {
-            if (!this.validateUserName() && !this.validateUserEmail() && !this.validateUserPassword()) {
+        async registerUser() {
+            if (!this.isValidUserName() && !this.isValidUserEmail() && !this.isValidPassword()) {
                 return
             }
 
-            await apolloClient.mutate({ mutation: gql`mutation ($password: String!, $email: String!, $name: String!) {
+            const user = await apolloClient.mutate({ mutation: gql`mutation ($password: String!, $email: String!, $name: String!) {
                 signUpUser(password: $password, email: $email, name: $name) {
                     token
                 }}`,
@@ -128,46 +106,47 @@ export default {
                     email: this.user.email, 
                     password: this.user.password,
                 },
-            }).then(async (res) => {
-                const token = res.data.signUpUser.token;
-
-                if (!token) {
-                    return false
-                }
-
-                await saveTokenInLocalStorage(token)
-                return true
             })
+            const token = user.data.signUpUser.token;
+
+            if (!token) {
+                this.isInvalidForm = true
+                return
+            }
+
+            this.isInvalidForm = false
+            await this.saveTokenInLocalStorage(token)
+            this.redirectToHome()
         },
-        saveTokenInLocalStorage: ({ token }) => {
+        saveTokenInLocalStorage(token) {
             localStorage.setItem("user", token)
         },
-        validateUserName() {
+        isValidUserName() {
             if (this.user.name.length < 3) {
-                isNameAlert = true
+                this.isNameAlert = true
                 return false
             }
 
-            isNameAlert = false
+            this.isNameAlert = false
             return true
         },
-        validateUserEmail() {
+        isValidUserEmail() {
             const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             if (this.user.email.length < 3 || !regexEmail.test(String(this.user.email).toLowerCase())) {
-                isEmailAlert = true
+                this.isEmailAlert = true
                 return false
             }
 
-            isEmailAlert = false
+            this.isEmailAlert = false
             return true
         },
-        validateUserPassword() {
+        isValidPassword() {
             if (this.user.password.length < 3) {
-                isPasswordAlert = true
+                this.isPasswordAlert = true
                 return false
             }
 
-            isPasswordAlert = false
+            this.isPasswordAlert = false
             return true
         },
     }
