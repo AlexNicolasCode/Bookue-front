cdf9b907691ad2d1b258359cb26d72af2eb1972c
AlexNicolasCode <alexoliveira604@gmail.com>
2021-11-02 00:38:08 -0300

    feat: user can navigate between screen using header

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -2,16 +2,20 @@
     <header>
         <LoginAndRegisterButton v-if="!isLogged" :border="isBorder" :title="title" :isHeader="true"/>
         <div v-if="isLogged" class="home">
-            <button v-if="!isHome" class="home__back-button">
-                <svg width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4.95313 9.28L0.133125 4.68L4.95313 0.0999994H7.25313L2.41313 4.68L7.25313 9.28H4.95313Z" fill="#04B2D9"/>
-                </svg>
-            </button>
+            <router-link to="/home">
+                <button v-if="!isHome" class="home__back-button">
+                    <svg width="8" height="10" viewBox="0 0 8 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4.95313 9.28L0.133125 4.68L4.95313 0.0999994H7.25313L2.41313 4.68L7.25313 9.28H4.95313Z" fill="#04B2D9"/>
+                    </svg>
+                </button>
+            </router-link>
             <BrandName />
-            <svg v-if="isHome" class="home__addTask" width="30" height="30" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <line y1="8" x2="16" y2="8" stroke="#04B2D9" stroke-width="2"/>
-                <line x1="8" y1="16" x2="8" stroke="#04B2D9" stroke-width="2"/>
-            </svg>
+            <router-link to="/add-book">
+                <svg v-if="isHome" class="home__addTask" width="30" height="30" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line y1="8" x2="16" y2="8" stroke="#04B2D9" stroke-width="2"/>
+                    <line x1="8" y1="16" x2="8" stroke="#04B2D9" stroke-width="2"/>
+                </svg>
+            </router-link>
         </div>
     </header>
 </template>
