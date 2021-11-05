67bd8480ee79ccb862d6766bb9a362a1d2fcb060
AlexNicolasCode <alexoliveira604@gmail.com>
2021-11-04 21:06:33 -0300

    refactor: user token must stay in cookies of browser

--- src/services/Apollo/ApolloClient.js
+++ src/services/Apollo/ApolloClient.js
@@ -1,18 +1,27 @@
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context';
+import Cookies from 'js-cookie';
 
 const httpLink = createHttpLink({
-  uri: new URL(process.env.BOOKUE_API),
-  headers: {
-    'Authorization': localStorage.getItem("user") ?? null
-  }
+  uri: new URL(import.meta.env.VITE_BOOKUE_API),
 })
+
+const token = Cookies.get("user")
+const authLink = setContext((_, { headers }) => {
+  return {
+   headers: {
+    ...headers,
+    Authorization: token ?? '',
+   },
+  }
+ })
 
 const cache = new InMemoryCache()
 
 export const apolloClient = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache,
 })
 
