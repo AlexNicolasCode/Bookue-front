c2a5a4ecca7d8b02228d8a97a49acc345b02a92f
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-08 00:53:53 -0300

    feat: adapted mocked cookie manager to include set method

--- tests/infra/mocks/mock.load.auth.token.ts
+++ tests/infra/mocks/mock.load.auth.token.ts
@@ -4,5 +4,6 @@ import cookieManager from "js-cookie";
 export const mockCookieManager = (value: string = faker.datatype.uuid()): jest.Mocked<typeof cookieManager> => {
     const mockedCookieManager = cookieManager as jest.Mocked<typeof cookieManager>
     jest.spyOn(mockedCookieManager, 'get').mockClear().mockReturnValueOnce(value as any)
+    jest.spyOn(mockedCookieManager, 'set').mockClear().mockReturnValueOnce(undefined)
     return mockedCookieManager
 }
