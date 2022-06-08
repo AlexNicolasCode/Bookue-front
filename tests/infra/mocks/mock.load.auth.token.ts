e654da4a43b67af527429c9b452ed2929852b3f2
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-07 23:36:04 -0300

    feat: cookieManager, of js-cookie lib, was mocked

---
+++ tests/infra/mocks/mock.load.auth.token.ts
@@ -0,0 +1,8 @@
+import { faker } from "@faker-js/faker";
+import cookieManager from "js-cookie";
+
+export const mockCookieManager = (value: string = faker.datatype.uuid()): jest.Mocked<typeof cookieManager> => {
+    const mockedCookieManager = cookieManager as jest.Mocked<typeof cookieManager>
+    jest.spyOn(mockedCookieManager, 'get').mockClear().mockReturnValueOnce(value as any)
+    return mockedCookieManager
+}
