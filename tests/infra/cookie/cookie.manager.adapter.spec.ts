b98b08e784a76f6ce17eda405af792108d851229
Alex Nicolas <alexoliveira604@gmail.com>
2022-06-08 00:58:38 -0300

    refactor: CookieManagerAdapter should call cookieManager with correct values

--- tests/infra/cookie/cookie.manager.adapter.spec.ts
+++ tests/infra/cookie/cookie.manager.adapter.spec.ts
@@ -4,34 +4,49 @@ import { CookieManagerAdapter } from "@/infra/cookie";
 import { faker } from "@faker-js/faker";
 
 describe('CookieManagerAdapter', () => {
-    test('should call cookieManager with correct key', async () => {
-        const fakeKey = faker.random.word()
-        const sut = new CookieManagerAdapter()
-        const mockedCookieManager = mockCookieManager()
-        
-        await sut.load(fakeKey)
-
-        expect(mockedCookieManager.get).toHaveBeenCalledWith(fakeKey)
+    describe('load()', () => {
+        test('should call cookieManager with correct key', async () => {
+            const fakeKey = faker.random.word()
+            const sut = new CookieManagerAdapter()
+            const mockedCookieManager = mockCookieManager()
+            
+            await sut.load(fakeKey)
+    
+            expect(mockedCookieManager.get).toHaveBeenCalledWith(fakeKey)
+        })
+    
+        test('should return null if not found value', async () => {
+            const fakeKey = faker.random.word()
+            const sut = new CookieManagerAdapter()
+            mockCookieManager(null)
+            
+            const value = await sut.load(fakeKey)
+    
+            expect(value).toBeFalsy()
+        })
+    
+        test('should return correct value on success', async () => {
+            const fakeKey = faker.random.word()
+            const fakeToken = faker.datatype.uuid()
+            const sut = new CookieManagerAdapter()
+            mockCookieManager(fakeToken)
+            
+            const value = await sut.load(fakeKey)
+    
+            expect(value).toBe(fakeToken)
+        })
     })
 
-    test('should return null if not found value', async () => {
-        const fakeKey = faker.random.word()
-        const sut = new CookieManagerAdapter()
-        mockCookieManager(null)
-        
-        const value = await sut.load(fakeKey)
+    describe('set()', () => {
+        test('should call cookieManager with correct values', async () => {
+            const fakeKey = faker.random.word()
+            const fakeValue = faker.datatype.uuid()
+            const sut = new CookieManagerAdapter()
+            const mockedCookieManager = mockCookieManager()
 
-        expect(value).toBeFalsy()
-    })
+            await sut.set(fakeKey, fakeValue)
 
-    test('should return correct value on success', async () => {
-        const fakeKey = faker.random.word()
-        const fakeToken = faker.datatype.uuid()
-        const sut = new CookieManagerAdapter()
-        mockCookieManager(fakeToken)
-        
-        const value = await sut.load(fakeKey)
-
-        expect(value).toBe(fakeToken)
+            expect(mockedCookieManager.set).toHaveBeenCalledWith(fakeKey, fakeValue)
+        })
     })
 })
