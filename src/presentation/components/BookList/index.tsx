6721d9b5b6722ddc0aebb922f177034c51c4a5f4
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-09 14:35:42 -0300

    feat: user should see styled BookList when access book's feed

---
+++ src/presentation/components/BookList/index.tsx
@@ -0,0 +1,10 @@
+import { Card } from "../Card";
+import { ListStyled } from "./styles";
+
+export function BookList() {
+    return (
+        <ListStyled>
+            <Card />
+        </ListStyled>
+    )
+}
---
+++ src/presentation/components/BookList/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ListStyled = styled.article`
+    padding: 0.5rem 1rem;
+`
+
+export { ListStyled }
