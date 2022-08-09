31ba75b31a69e8ea49e2089a9569796abbe5104e
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-09 20:45:58 -0300

    feat: input component should be able to set state and itself adapt to diferent input types

--- src/presentation/components/Input/index.tsx
+++ src/presentation/components/Input/index.tsx
@@ -1,7 +1,26 @@
+import { Dispatch, SetStateAction } from "react";
+
 import { InputStyled } from "./styles";
 
-export function Input() {
+type InputProps = {
+    type: string
+    placeholder: string
+    setState: Dispatch<SetStateAction<string>>
+    value: string
+}
+
+export function Input({ 
+    type,
+    placeholder,
+    setState,
+    value,
+}: InputProps) {
     return (
-        <InputStyled type="email" placeholder="Email"/>
+        <InputStyled 
+            type={type}
+            placeholder={placeholder}
+            onChange={(event) => setState(event.target.value)}
+            value={value}
+        />
     )
 }
