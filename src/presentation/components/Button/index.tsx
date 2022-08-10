5e23d35f4ad0aeac213d1dfe029b18a780cf73da
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-09 21:16:46 -0300

    feat: default button should accept onClick attribut

--- src/presentation/components/Button/index.tsx
+++ src/presentation/components/Button/index.tsx
@@ -1,12 +1,15 @@
+import { MouseEventHandler } from "react";
+
 import { ButtonStyled } from "./styles";
 
 type ButtonProps = {
     value?: string
+    onClick?: MouseEventHandler<HTMLButtonElement>
 }
 
-export function Button({ value }: ButtonProps) {
+export function Button({ value, onClick }: ButtonProps) {
     return (
-        <ButtonStyled>
+        <ButtonStyled onClick={onClick}>
             {value}
         </ButtonStyled>
     )
