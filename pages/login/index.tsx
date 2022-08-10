ee28834b0f8281e5f8210293417ee45d0c9742f8
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-10 00:47:36 -0300

    feat: user should see sign up button when access login screen

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,8 +1,16 @@
-import { Login } from "@/presentation/components/Login"
+import { Button, Login } from "@/presentation/components"
+
+import { HeaderStyled } from "./styled"
 
 function LoginPage() {
     return (
-        <Login />
+        <>
+            <HeaderStyled>
+                <Button borded value="Sign Up"/>
+            </HeaderStyled>
+
+            <Login />
+        </>
     )
 }
 
---
+++ pages/login/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.section`
+    padding: 0.5rem 0.5rem;
+    position: absolute;
+    right: 0;
+    z-index: 1;
+`
+
+export {
+    HeaderStyled,
+}
