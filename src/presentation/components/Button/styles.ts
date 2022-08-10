2e49bf3d7e7b1ee03915401899117262775157d1
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-09 23:54:17 -0300

    feat: button from login form should follow styles of design documentation

--- src/presentation/components/Button/styles.ts
+++ src/presentation/components/Button/styles.ts
@@ -4,6 +4,8 @@ import { globalColors } from "@/presentation/styles/colors"
 
 const ButtonStyled = styled.button`
     background-color: ${globalColors.primary};
+    border-radius: 2.5px;
+    color: ${globalColors.white};
     width: 125px;
     height: 40px;
 `
