04da0aad5c329752630e2d378357b998d74b8c8a
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-11 00:19:50 -0300

    refactor: applied sut pattern to avoid code duplication

--- tests/validation/validatiors/required.field.validation.spec.ts
+++ tests/validation/validatiors/required.field.validation.spec.ts
@@ -3,10 +3,12 @@ import { faker } from "@faker-js/faker"
 import { RequiredFieldError } from "@/validation/errors";
 import { RequiredFieldValidation } from "@/validation/validators";
 
+const makeSut = (field: string): RequiredFieldValidation => new RequiredFieldValidation(field)
+
 describe('RequiredFieldValidation', () => {
     test('Should return error if field is empty', () => {
         const field = faker.database.column()
-        const sut = new RequiredFieldValidation(field)
+        const sut = makeSut(field)
 
         const error = sut.validate({ [field]: '' })
 
@@ -15,7 +17,7 @@ describe('RequiredFieldValidation', () => {
 
     test('Should return falsy if field is not empty', () => {
         const field = faker.database.column()
-        const sut = new RequiredFieldValidation(field)
+        const sut = makeSut(field)
 
         const error = sut.validate({ [field]: faker.random.word() })
 
