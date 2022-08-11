7aa17735ef2666ec7aa89f68d99c5ef959910b25
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-10 23:23:15 -0300

    feat: form component should be able to accept onSubmit attribute

--- src/presentation/components/Form/index.tsx
+++ src/presentation/components/Form/index.tsx
@@ -1,12 +1,15 @@
+import { FormEvent } from "react";
+
 import { FormStyled } from "./styles";
 
 type FormProps = {
     children: JSX.Element | JSX.Element[]
+    onSubmit: (event: FormEvent<HTMLFormElement>) => void
 }
 
-function Form({ children }: FormProps) {
+function Form({ children, onSubmit }: FormProps) {
     return (
-        <FormStyled>
+        <FormStyled onSubmit={onSubmit}>
             {children}
         </FormStyled>
     )
