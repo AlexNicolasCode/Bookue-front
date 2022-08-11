7ece51904c593cb5b47194ed83e20776fa90579a
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-11 00:58:21 -0300

    feat: ValidationBuilder was created

--- tests/main/builders/validation.builder.spec.ts
+++ tests/main/builders/validation.builder.spec.ts
@@ -2,36 +2,11 @@ import { faker } from "@faker-js/faker";
 
 import { FieldValidation } from "@/validation/protocols";
 import { EmailValidation, RequiredFieldValidation } from "@/validation/validators";
-
-class ValidationBuilder {
-    private constructor (
-        private readonly fieldName: string,
-        private readonly validations: FieldValidation[],
-    ) {}
-
-    static field (fieldName: string): ValidationBuilder {
-        return new ValidationBuilder(fieldName, [])
-    }
-
-    required (): ValidationBuilder {
-        this.validations.push(new RequiredFieldValidation(this.fieldName))
-        return this
-    }
-
-    email (): ValidationBuilder {
-        this.validations.push(new EmailValidation(this.fieldName))
-        return this
-    }
-
-    build (): FieldValidation[] {
-        return this.validations
-    }
-}
+import { ValidationBuilder as sut } from "@/main/builders/validation.builder";
 
 describe('ValidationBuilder', () => {
     test('Should return RequiredFieldValidation', () => {
         const field = faker.database.column()
-        const sut = ValidationBuilder
 
         const validations = sut.field(field).required().build()
 
@@ -40,7 +15,6 @@ describe('ValidationBuilder', () => {
 
     test('Should return EmailValidation', () => {
         const field = faker.database.column()
-        const sut = ValidationBuilder
 
         const validations = sut.field(field).email().build()
 
@@ -49,7 +23,6 @@ describe('ValidationBuilder', () => {
 
     test('Should return a list of validations', () => {
         const field = faker.database.column()
-        const sut = ValidationBuilder
 
         const validations = sut.field(field).required().email().build()
 
