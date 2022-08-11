14b5a27d9baa3160bb629bc18877de9ef018cf29
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-11 01:24:35 -0300

    feat: LoginValidation was created

---
+++ src/main/factory/validation/login.validation.factory.ts
@@ -0,0 +1,7 @@
+import { ValidationBuilder as Builder } from "@/main/builders";
+import { ValidationComposite } from "@/main/composites";
+
+export const makeLoginValidation = () => ValidationComposite.build([
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().email().build(),
+])
