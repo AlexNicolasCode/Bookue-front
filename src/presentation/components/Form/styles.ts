3a3ff65dbe49654ae0eaf502e1b6daeb9a81f587
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-10 23:10:59 -0300

    feat: FormStyled should use form tag

--- src/presentation/components/Form/styles.ts
+++ src/presentation/components/Form/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const FormStyled = styled.section`
+const FormStyled = styled.form`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
