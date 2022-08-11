100f359a05d6f15c7960dd24e7e1e23c537de2e9
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-11 01:20:22 -0300

    feat: ValidationComposite was created

--- tests/main/composites/validation.composite.spec.ts
+++ tests/main/composites/validation.composite.spec.ts
@@ -1,25 +1,7 @@
-import { Validation } from "@/presentation/protocols";
-import { FieldValidation } from "@/validation/protocols";
 import { faker } from "@faker-js/faker";
+
+import { ValidationComposite } from "@/main/composites";
 import { FieldValidationSpy } from "tests/validation/mocks";
-
-export class ValidationComposite implements Validation {
-    private constructor (private readonly validatiors: FieldValidation[]) {}
-
-    static build (validatiors: FieldValidation[]): ValidationComposite {
-        return new ValidationComposite(validatiors)
-    }
-
-    validate (fieldName: string, input: object): string {
-        const validatiors = this.validatiors.filter(v => v.field === fieldName)
-        for (const validator of validatiors) {
-            const error = validator.validate(input)
-            if (error) {
-                return error.message
-            }
-        }
-    }
-}
 
 type SutTypes = {
     sut: ValidationComposite
