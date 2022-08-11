5e15dbe8ac6d76bd1b2542441effc4e0fa6207a1
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-10 23:06:02 -0300

    feat: user should be alerted when try register account if used email

--- src/presentation/components/Register/index.tsx
+++ src/presentation/components/Register/index.tsx
@@ -5,13 +5,16 @@ import {
     Input,
     Logo,
     Button,
+    Alert,
 } from "src/presentation/components"
-import { AddAccount } from "@/domain/usecases"
 
 import { RegisterStyled } from "./styled"
+
+import { AddAccount } from "@/domain/usecases"
 import { makeRemoteAddAccount } from "@/main/factory/usecases"
 
 function Register() {
+    const [alert, setAlert] = useState<string>("")
     const [userForm, setUserForm] = useState<AddAccount.Params>({
         name: '',
         email: '',
@@ -22,14 +25,19 @@ function Register() {
     const setEmail = (text: string) => setUserForm({...userForm, email: text})
     const setPassword = (text: string) => setUserForm({...userForm, password: text})
     const setPasswordConfirmation = (text: string) => setUserForm({...userForm, passwordConfirmation: text})
-    const registerUser = () => {
-        const remoteAddAccount = makeRemoteAddAccount()
-        remoteAddAccount.add({
-            name: userForm.name,
-            email: userForm.email,
-            password: userForm.password,
-            passwordConfirmation: userForm.passwordConfirmation,
-        })
+    const registerUser = async () => {
+        try {
+            const remoteAddAccount = makeRemoteAddAccount()
+            await remoteAddAccount.add({
+                name: userForm.name,
+                email: userForm.email,
+                password: userForm.password,
+                passwordConfirmation: userForm.passwordConfirmation,
+            })
+        } catch (error) {
+            console.log(error.message)
+            setAlert(error.message)
+        }
     }
 
     return (
@@ -61,6 +69,13 @@ function Register() {
                     setState={setPasswordConfirmation}
                     value={userForm.passwordConfirmation}
                 />
+
+                {
+                    alert.length > 0 &&
+                    <Alert>
+                        {alert}                
+                    </Alert>
+                }
 
                 <Button
                     onClick={registerUser}
