d944b0bd890a6170537b760a751ec1f94cfa9a99
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-18 14:09:34 -0300

    feat: authenticated user should be redirect to home page when try access sign up page

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router"
+import { GetServerSideProps } from "next"
 
 import { UnloggedHeader, Register } from "@/presentation/components"
+import { makeCookieManagerAdapter } from "@/main/factory/cookie"
 
 function SignUpPage() {
     const router = useRouter()
@@ -19,6 +21,19 @@ function SignUpPage() {
             <Register />
         </>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const cookieManager = makeCookieManagerAdapter()
+    const token = await cookieManager.load('bookue-user')
+    if (token) {
+        return {
+            props: {},
+            redirect: {
+                destination: '/'
+            }
+        }
+    }
 }
 
 export default SignUpPage
