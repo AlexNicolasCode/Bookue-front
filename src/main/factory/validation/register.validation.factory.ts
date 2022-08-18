4b028a6dc1cbf052fe47161550637d2a2ee26281
Alex Nicolas <alexoliveira604@gmail.com>
2022-08-18 14:25:02 -0300

    feat: makeRegisterValidation factory was created

---
+++ src/main/factory/validation/register.validation.factory.ts
@@ -0,0 +1,9 @@
+import { ValidationBuilder as Builder } from "@/main/builders";
+import { ValidationComposite } from "@/main/composites";
+
+export const makeRegisterValidation = () => ValidationComposite.build([
+    ...Builder.field('name').required().build(),
+    ...Builder.field('email').required().email().build(),
+    ...Builder.field('password').required().build(),
+    ...Builder.field('passwordConfirmation').required().build(),
+])
