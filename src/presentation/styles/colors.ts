07c38119168df6018d6efbe1aa11058551452fd7
Alex Nicolas <alexoliveira604@gmail.com>
2022-09-13 23:29:56 -0300

    feat: change gray color to improve text readability

--- src/presentation/styles/colors.ts
+++ src/presentation/styles/colors.ts
@@ -1,7 +1,7 @@
 const globalColors = {
     primary: '#04b2d9',
     white: '#f2f2f2',
-    gray: '#e1e1e1',
+    gray: '#8c8c8c',
     warn: '#d94404',
     dark: '#0c0c0c',
     placeholder: '#535353',
