32b03b7d7ea5c8fecc246a460c563af193ac8868
Alex Nicolas <alexoliveira604@gmail.com>
2022-12-14 12:23:10 -0300

    feat: password and passwordConfirmation fields should be compareted to avoid sign up error

--- src/main/factory/validation/register.validation.factory.ts
+++ src/main/factory/validation/register.validation.factory.ts
@@ -4,6 +4,6 @@ import { ValidationComposite } from "@/main/composites";
 export const makeRegisterValidation = () => ValidationComposite.build([
     ...Builder.field('name').required().build(),
     ...Builder.field('email').required().email().build(),
-    ...Builder.field('password').required().build(),
     ...Builder.field('passwordConfirmation').required().build(),
+    ...Builder.field('password').required().sameAs('passwordConfirmation').build(),
 ])
