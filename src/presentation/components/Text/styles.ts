51948a5d6ffb0f46ba2956950996701e233b1ad4
Alex Nicolas <alexoliveira604@gmail.com>
2023-02-21 13:30:41 -0300

    feat: Text component was created

---
+++ src/presentation/components/Text/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+import { globalColors } from "@/presentation/styles/colors"
+
+const TextStyled = styled.p`
+    color: ${globalColors.gray};
+    font-size: 12px;
+    width: 125px;
+    height: 40px;
+
+`
+
+export {
+    TextStyled,
+}
