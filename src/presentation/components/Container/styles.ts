32b1474d772f5180e5e5e9d9ca30934ddae0fd8d
Alex Nicolas <alexoliveira604@gmail.com>
2023-02-21 14:15:35 -0300

    feat: Container can have margin top settled when necessary

--- src/presentation/components/Container/styles.ts
+++ src/presentation/components/Container/styles.ts
@@ -2,10 +2,22 @@ import styled from "styled-components";
 
 type ContainerProps = {
     flex?: boolean
+    smallMarginTop?: boolean
+    mediumMarginTop?: boolean
+    largeMarginTop?: boolean
+    extraLargeMarginTop?: boolean
+}
+
+const getMarginTop = (props: ContainerProps) => {
+    if (props.smallMarginTop) return '8px'
+    if (props.mediumMarginTop) return '16px'
+    if (props.largeMarginTop) return '32px'
+    if (props.extraLargeMarginTop) return '64px'
+    return '10%'
 }
 
 const ContainerStyled = styled.section`
-    margin-top: 10%;
+    margin-top: ${(props: ContainerProps) => getMarginTop(props)};
     display: ${(props: ContainerProps) => props.flex ? 'flex' : 'block'};
 `
 
