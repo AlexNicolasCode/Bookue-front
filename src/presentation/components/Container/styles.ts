aec3b2d6a28d5de918ec9491e1fe95fa79cb08c2
Alex Nicolas <alexoliveira604@gmail.com>
2023-02-21 13:47:26 -0300

    feat: Container can be horizontal or vertical according to props received

--- src/presentation/components/Container/styles.ts
+++ src/presentation/components/Container/styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 
+type ContainerProps = {
+    flex?: boolean
+}
+
 const ContainerStyled = styled.section`
-    margin-top: 10%
+    margin-top: 10%;
+    display: ${(props: ContainerProps) => props.flex ? 'flex' : 'block'};
 `
 
 export {
