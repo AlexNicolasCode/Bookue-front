8ef44af3ac63536cd79cc8090146956f71021d9a
Alex Nicolas <alexoliveira604@gmail.com>
2023-02-26 12:25:44 -0300

    refactor: factory name update

--- src/main/factory/usecases/remote.add.book.factory.ts
+++ src/main/factory/usecases/remote.add.book.factory.ts
@@ -2,4 +2,4 @@ import { RemoteAddBook } from '@/data/usecases';
 import { AddBook } from '@/domain/usecases';
 import { makeAxiosHttpClient, makeBookueApiUrl } from '../http';
 
-export const makeRemoteAddAccount = (): AddBook => new RemoteAddBook(makeBookueApiUrl(''), makeAxiosHttpClient());
+export const makeRemoteAddBook = (): AddBook => new RemoteAddBook(makeBookueApiUrl(''), makeAxiosHttpClient());
