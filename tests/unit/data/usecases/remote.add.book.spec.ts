aef90be04ad719acc822def308c258b8e1499a59
Alex Nicolas <alexoliveira604@gmail.com>
2023-02-26 12:21:41 -0300

    feat: RemoteAddBook was created

--- tests/unit/data/usecases/remote.add.book.spec.ts
+++ tests/unit/data/usecases/remote.add.book.spec.ts
@@ -1,6 +1,6 @@
-import { HttpClient, HttpStatusCode } from "@/data/protocols/http";
+import { HttpStatusCode } from "@/data/protocols/http";
+import { RemoteAddBook } from "@/data/usecases";
 import { UnexpectedError } from "@/domain/errors";
-import { AddBook } from "@/domain/usecases";
 
 import { faker } from "@faker-js/faker";
 
@@ -11,56 +11,6 @@ type HttpResponseAddBook = {
   data: {
     addBook: {
         id: string
-    }
-  }
-}
-
-export class RemoteAddBook implements AddBook {
-  constructor(
-        private readonly url: string,
-        private readonly httpClient: HttpClient<HttpResponseAddBook>,
-  ) {}
-
-  async add(params: AddBook.Params): Promise<undefined> {
-    const httpResponse = await this.httpClient.request({
-      url: this.url,
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        query: `
-            mutation Mutation(
-                $title: String!,
-                $author: String!,
-                $pages: String!,
-                $accessToken: String!,
-                $description: String,
-                $currentPage: String
-            ) {
-                addBook(
-                    title: $title, 
-                    author: $author, 
-                    pages: $pages, 
-                    accessToken: $accessToken, 
-                    description: $description, 
-                    currentPage: $currentPage
-                ) {
-                    id
-                }
-            }
-          `,
-
-        variables: {  
-            title: params.title,
-            author: params.author,
-            pages: params.pages,
-            description: params.description,
-            currentPage: params.currentPage ?? 0,
-        },
-      })
-    });
-    switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return;
-      default: throw new UnexpectedError();
     }
   }
 }
