1e7fa456df5462e5ff05d2e2bf6424c7922e5903
Alex Nicolas <alexoliveira604@gmail.com>
2023-04-06 14:45:53 -0300

    feat: book should be added when user submit current page form

--- pages/book/add/current-page/index.tsx
+++ pages/book/add/current-page/index.tsx
@@ -3,12 +3,25 @@ import { GetServerSideProps } from "next"
 
 import { Header, Input, Form, Container, SubmitButton, Text } from "@/presentation/components"
 import { useBookForm } from "@/presentation/hooks"
+import { makeRemoteAddBook } from "@/main/factory/usecases"
 
 function AddBookCurrentPagePage() {
     const { bookForm, setField } = useBookForm()
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
         event.preventDefault()
+        try {
+            const addBook = makeRemoteAddBook()
+            await addBook.add({
+                title: bookForm.title.text,
+                author: bookForm.author.text,
+                currentPage: Number(bookForm.currentPage.text) ?? 0,
+                description: bookForm.description.text,
+                pages: Number(bookForm.pages.text),
+            })
+        } catch (error) {
+            
+        }
     }
     
     const renderCurrentPageForm = () => (
