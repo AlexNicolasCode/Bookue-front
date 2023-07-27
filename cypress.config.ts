4c74241ea2a79c9d201d3a2e5a30655b860fc92f
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-27 00:53:13 -0300

    chore: update specPattern path

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress"
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    specPattern: "tests/e2e/*.cy.test.ts"
+    specPattern: "tests/e2e/integration/*.cy.test.ts",
+    supportFile: "tests/e2e/support"
   },
 })
