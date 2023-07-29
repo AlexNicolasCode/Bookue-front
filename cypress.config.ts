d7a95576dfce97aa9c1c2b41e418f84d0c17c9cc
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-29 19:58:14 -0300

    chore: cypress config update to enable to create temporary server

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,3 +1,4 @@
+import express from "express"
 import { defineConfig } from "cypress"
 
 export default defineConfig({
@@ -5,9 +6,34 @@ export default defineConfig({
     baseUrl: "http://localhost:3000",
     specPattern: "tests/e2e/cypress/integration/*.cy.test.ts",
     supportFile: "tests/e2e/cypress/support",
-    fixturesFolder: "tests/e2e/cypress/fixtures"
+    fixturesFolder: "tests/e2e/cypress/fixtures",
+    setupNodeEvents(on, config) {
+      let server
+  
+      on('task', {
+        setServerConfig({ baseUrl, statusCode, body }) {
+          server = express()
+          server.post(baseUrl, (req, res) => {
+            res.status(statusCode).send(body)
+          })
+          return null
+        },
+
+        startServer() {
+          if (!server) return
+          server.listen(8000)
+          return null
+        },
+
+        closeServer() {
+          if (!server) return
+          server.close()
+          return null
+        }
+      })
+    }
   },
   env: {
-    baseApiURL: 'http://localhost:8000/graphql',
+    baseApiURL: 'http://localhost:8000/graphql/',
   }
 })
