a9a939e733b04fe534bca7d0803cb71b89654373
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-29 15:29:44 -0300

    chore: cypress config update

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "cypress"
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
-    specPattern: "tests/e2e/integration/*.cy.test.ts",
-    supportFile: "tests/e2e/support",
-    fixturesFolder: "tests/e2e/fixtures"
+    specPattern: "tests/e2e/cypress/integration/*.cy.test.ts",
+    supportFile: "tests/e2e/cypress/support",
+    fixturesFolder: "tests/e2e/cypress/fixtures"
   },
   env: {
     baseApiURL: 'http://localhost:8000/graphql',
