b4411e4b7f196b3aaddb6090f277097902420dde
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-30 17:53:12 -0300

    test: RemoteLoadBook should throw if HttpClient throws

--- tests/data/usecases/remote.load.book.spec.ts
+++ tests/data/usecases/remote.load.book.spec.ts
@@ -4,6 +4,7 @@ import { HttpClient } from "@/data/protocols/http";
 import { LoadBook } from "@/domain/usecases";
 
 import { HttpClientSpy } from "@/tests/data/mocks";
+import { throwError } from "@/tests/main/domain/mocks/test.helpers";
 
 class RemoteLoadBook implements LoadBook {
     constructor(
@@ -45,23 +46,50 @@ export type HttpResponseLoadBook = {
     }
 }
 
+type SutTypes = {
+    sut: RemoteLoadBook
+    httpClientSpy: HttpClientSpy
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy<HttpResponseLoadBook>();
+  const sut = new RemoteLoadBook(url, httpClientSpy);
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
+
 describe('RemoteLoadBook', () => {
-  test('should call HttpClient with correct values', async () => {
-    const url = faker.internet.url();
-    const httpClientSpy = new HttpClientSpy()
-    const sut = new RemoteLoadBook(url, httpClientSpy);
-    const fakeRequest = {
-        accessToken: faker.datatype.uuid(),
-        bookId: faker.datatype.uuid(),
-    };
+    let fakeRequest
 
-    await sut.loadBook(fakeRequest);
+    beforeEach(() => {
+        fakeRequest = {
+            accessToken: faker.datatype.uuid(),
+            bookId: faker.datatype.uuid(),
+        };
+    })
 
-    expect(httpClientSpy.url).toBe(url);
-    expect(httpClientSpy.method).toBe('post');
-    expect(httpClientSpy.headers).toStrictEqual({
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${fakeRequest.accessToken}`,
+    test('should call HttpClient with correct values', async () => {
+        const url = faker.internet.url();
+        const { sut, httpClientSpy } = makeSut(url);
+
+        await sut.loadBook(fakeRequest);
+
+        expect(httpClientSpy.url).toBe(url);
+        expect(httpClientSpy.method).toBe('post');
+        expect(httpClientSpy.headers).toStrictEqual({
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${fakeRequest.accessToken}`,
+        });
     });
-  });
+
+    test('should throw if HttpClient throws', async () => {
+        const { sut, httpClientSpy } = makeSut();
+        jest.spyOn(httpClientSpy, 'request').mockImplementationOnce(throwError)
+
+        const promise = sut.loadBook(fakeRequest);
+
+        await expect(promise).rejects.toThrow()
+    });
 })
