526746c59d56a6dff0956f6b2b440b4c2bd45fb1
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-30 00:09:50 -0300

    feat: book notes screen was created

---
+++ pages/book/[slug]/note/index.tsx
@@ -0,0 +1,28 @@
+import { GetServerSideProps } from "next"
+
+import { Header } from "@/presentation/components"
+
+function BookNotesPage() {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const accessToken = context.req.cookies['bookue-user']
+    if (!accessToken) {
+        return {
+            props: {},
+            redirect: {
+                destination: '/login'
+            }
+        }
+    }
+    return {
+        props: {},
+    }
+}
+
+export default BookNotesPage
