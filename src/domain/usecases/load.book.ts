e9091c96a33e2303f77bebc8cea5b977d26c460b
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-30 17:42:55 -0300

    feat: LoadBook usecase was created

---
+++ src/domain/usecases/load.book.ts
@@ -0,0 +1,13 @@
+import { BookModel } from "../models"
+
+export interface LoadBook {
+    loadBook: (params: LoadBook.Params) => Promise<LoadBook.Result | []>
+}
+
+export namespace LoadBook {
+    export type Params = {
+        accessToken: string
+        bookId: string
+    }
+    export type Result = BookModel
+}
