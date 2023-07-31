8d5fda843199c19fa86c1e672abe98806093ccc2
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-31 20:16:22 -0300

    feat: edit book validation factory was created

---
+++ src/main/factory/validation/edit.book.validation.factory.ts
@@ -0,0 +1,10 @@
+import { ValidationBuilder as Builder } from "@/main/builders";
+import { ValidationComposite } from "@/main/composites";
+
+export const makeEditBookValidation = () => ValidationComposite.build([
+    ...Builder.field('title').required().build(),
+    ...Builder.field('author').required().email().build(),
+    ...Builder.field('description').required().build(),
+    ...Builder.field('currentPage').required().lessThanEqual('pages').build(),
+    ...Builder.field('pages').required().greaterThanEqual('currentPage').build(),
+])
