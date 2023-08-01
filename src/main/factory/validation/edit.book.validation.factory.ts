3b3e47b804b3d72f8ec2fd78bc597bd2b1b0e16b
Alex Nicolas <alexoliveira604@gmail.com>
2023-07-31 21:12:27 -0300

    fix: remove email validation to author field to enable user edit this field normaly

--- src/main/factory/validation/edit.book.validation.factory.ts
+++ src/main/factory/validation/edit.book.validation.factory.ts
@@ -3,7 +3,7 @@ import { ValidationComposite } from "@/main/composites";
 
 export const makeEditBookValidation = () => ValidationComposite.build([
     ...Builder.field('title').required().build(),
-    ...Builder.field('author').required().email().build(),
+    ...Builder.field('author').required().build(),
     ...Builder.field('description').required().build(),
     ...Builder.field('currentPage').required().lessThanEqual('pages').build(),
     ...Builder.field('pages').required().greaterThanEqual('currentPage').build(),
