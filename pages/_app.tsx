ce1e0e0f5eda23b451d9cf0c76c1f5c1581e3049
Alex Nicolas <alexoliveira604@gmail.com>
2023-08-12 01:15:15 -0300

    feat: alert context provider was applied to enable change alert text and active when necessary in any screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app'
 
+import { AlertProvider } from '@/presentation/contexts'
+import { useAlert } from '@/presentation/hook'
+
 import '@/presentation/styles/reset.styles.css'
 import '@/presentation/styles/global.styles.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+    const { renderAlert } = useAlert()
     return (
-        <Component {...pageProps} />
+        <AlertProvider>
+            <Component {...pageProps} />
+            {renderAlert()}
+        </AlertProvider>
     )
 }
