c653de334401457bc9d83cc04bb38244504dc3d9
Alex Nicolas <alexoliveira604@gmail.com>
2023-08-15 18:37:09 -0300

    feat: remove isActive attribute in Alert component to avoid receive useless param

--- src/presentation/components/Alert/index.tsx
+++ src/presentation/components/Alert/index.tsx
@@ -2,14 +2,13 @@ import { AlertContainerStyled, AlertStyled } from "./styles";
 
 type AlertProps = {
     children: string
-    isActive: boolean
     type: string
     testId?: string
 }
 
-function Alert({ children, isActive, type, testId }: AlertProps) {
+function Alert({ children, type, testId }: AlertProps) {
     return (
-        <AlertContainerStyled isActive={isActive} type={type}>
+        <AlertContainerStyled type={type}>
             <AlertStyled data-test-id={testId}>
                 {children}
             </AlertStyled>
