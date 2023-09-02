bac7c20428d93888a0cf6b5a80a5caf7fc66349e
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-02 15:28:21 -0300

    feat: styled-component stylization should be loaded when pages loaded to improve ux

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css?family=Poppins"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+export default class MyDocument extends Document {
+  static async getInitialProps(context) { 
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = context.renderPage;
+    try {
+      context.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await Document.getInitialProps(context);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            <link
+              href="https://fonts.googleapis.com/css?family=Poppins"
+              rel="stylesheet"
+            />
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
-
-export default MyDocument
