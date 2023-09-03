b74b97a210cfcbbd7b13643a0ef7fbf60bdf7fe2
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-03 13:57:44 -0300

    test: Add Book screen should greater than field error alert when submit form after fill currentPage field with a number with more than pages field value when authenticated

--- tests/e2e/cypress/integration/add.book.cy.test.ts
+++ tests/e2e/cypress/integration/add.book.cy.test.ts
@@ -1,5 +1,6 @@
 import { faker } from "@faker-js/faker"
-import { RequiredFieldError } from "../../../../src/validation/errors"
+
+import { GreaterThanFieldError, RequiredFieldError } from "../../../../src/validation/errors"
 
 describe('Add Book screen', () => {
   beforeEach(() => {
@@ -56,6 +57,20 @@ describe('Add Book screen', () => {
       cy.getByTestId('book-add-submit-form').click()
 
       cy.getByTestId('alert-message').contains(requiredFieldError, { matchCase: false })
+    })
+
+    it('Should greater than field error alert when submit form after fill currentPage field with a number with more than pages field value', () => {
+      const greaterThanFieldError = new GreaterThanFieldError('current page', 'pages').message
+      fakeBook.currentPage = fakeBook.pages + faker.datatype.number({ min: 1 })
+      cy.visit('/book/add/')
+
+      cy.getByTestId('book-add-title-field').type(fakeBook.title)
+      cy.getByTestId('book-add-pages-field').type(fakeBook.pages)
+      cy.getByTestId('book-add-description-field').type(fakeBook.description)
+      cy.getByTestId('book-add-currentPage-field').type(fakeBook.currentPage)
+      cy.getByTestId('book-add-submit-form').click()
+
+      cy.getByTestId('alert-message').contains(greaterThanFieldError, { matchCase: false })
     })
   })
 })
