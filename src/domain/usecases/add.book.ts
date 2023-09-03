c9237b2d21c59ce7d5561ab183dd016d7e19b4aa
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-03 14:28:32 -0300

    feat: AddBook usecase interface was created

---
+++ src/domain/usecases/add.book.ts
@@ -0,0 +1,14 @@
+export interface AddBook {
+    add: (params: AddBook.Params) => Promise<void>
+}
+
+export namespace AddBook {
+    export type Params = {
+        title: string
+        author: string
+        description: string
+        currentPage: string
+        pages: string
+        accessToken: string
+    }
+}
