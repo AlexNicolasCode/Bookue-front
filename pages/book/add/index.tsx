0951123a34ded0e754663ba4a53605e12bb0bab2
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-03 12:52:26 -0300

    feat: user should see all book fields when access add book screen

--- pages/book/add/index.tsx
+++ pages/book/add/index.tsx
@@ -1,24 +1,72 @@
-import { GetServerSideProps } from "next"
+import { FormEvent, useState } from "react"
 
-import { Header } from "@/presentation/components"
-import { BookModel } from "@/domain/models"
+import { Form, Header, Input, SubmitButton } from "@/presentation/components"
 
-export type AddBookPageProps = {
-    book: BookModel
+type BookField = {
+    fieldName: string
+    placeholder: string
+    isWrongFill: boolean
+    testId: string
+    value: string
 }
 
-function AddBookPage({ book }: AddBookPageProps) {
+const fieldNames = ['title', 'author', 'currentPage', 'pages', 'description']
+
+export default function AddBookPage() {
+    const [form, setForm] = useState<BookField[]>(
+        fieldNames.map((fieldName) => ({
+            fieldName: fieldName,
+            placeholder: fieldName,
+            isWrongFill: false,
+            value: '',
+            testId: `book-add-${fieldName}-field`,
+        }))
+    )
+
+    const getFieldType = (fieldName: string) => {
+        const fieldNameLowCase = fieldName.toLowerCase()
+        const isPageField = /page/.test(fieldNameLowCase)
+        return isPageField ? 'number' : 'text'
+    }
+
+    const setField = (fieldName: string, value: string): void => {
+        const updatedForm = form.map((field, index) => {
+            if(field.fieldName === fieldName) {
+                form[index].value = value
+                return form[index]
+            }
+            return form[index]
+        })
+        setForm(updatedForm)
+    }
+
+    const renderFields = () => {
+        const fields = form.map((field, index) => (
+            <Input
+                field={field.fieldName}
+                type={getFieldType(field.fieldName)}
+                placeholder={field.placeholder}
+                isWrongFill={field.isWrongFill} 
+                setState={setField}
+                testId={field.testId}
+                value={field.value}
+                key={index}
+            />
+        ))
+        return <>{fields}</>
+    }
+
+    const handleForm = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
+
     return (
         <>
             <Header/>
+            <Form onSubmit={handleForm}>
+                {renderFields()}
+                <SubmitButton text={'Save'} testId="book-add-submit-form"/>
+            </Form>
         </>
     )
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    return {
-        props: {},
-    }
-}
-
-export default AddBookPage
