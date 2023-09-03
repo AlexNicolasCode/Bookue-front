0de29ce7ca34e57dc10d354a8b5c2c7b9fc534db
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-03 13:53:40 -0300

    test: Add Book screen Should see required field error alert when submit form without fill description field when authenticated

--- tests/e2e/cypress/integration/add.book.cy.test.ts
+++ tests/e2e/cypress/integration/add.book.cy.test.ts
@@ -46,5 +46,16 @@ describe('Add Book screen', () => {
 
       cy.getByTestId('alert-message').contains(requiredFieldError, { matchCase: false })
     })
+
+    it('Should see required field error alert when submit form without fill description field', () => {
+      const requiredFieldError = new RequiredFieldError().message
+      cy.visit('/book/add/')
+
+      cy.getByTestId('book-add-title-field').type(fakeBook.title)
+      cy.getByTestId('book-add-pages-field').type(fakeBook.pages)
+      cy.getByTestId('book-add-submit-form').click()
+
+      cy.getByTestId('alert-message').contains(requiredFieldError, { matchCase: false })
+    })
   })
 })
