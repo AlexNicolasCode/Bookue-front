c65cc52a184c72d21ecbf61497223fa1233f0aac
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-07 15:05:57 -0300

    feat: MainContent shouldn't have direction styles to avoid broken implementation from another component

--- src/presentation/components/MainContent/styles.ts
+++ src/presentation/components/MainContent/styles.ts
@@ -3,7 +3,4 @@ import styled from "styled-components";
 export const MainContent = styled.main`
     height: 80vh;
     width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-itens: center;
 `
