ad34fd923458dc35b593739ad6ce0f8a1dfd1733
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-08 17:08:25 -0300

    test: Notes screen should change to delete mode when user click in delete mode icon when authenticated

--- tests/e2e/cypress/integration/notes.cy.test.ts
+++ tests/e2e/cypress/integration/notes.cy.test.ts
@@ -1,16 +1,32 @@
 import { faker } from "@faker-js/faker"
 
 describe('Notes screen', () => {
-  beforeEach(() => {
-    cy.viewport('iphone-x')
-  })
+    beforeEach(() => {
+      cy.viewport('iphone-x')
+    })
 
-  describe('when not authenticated', () => {
-    it('Should redirect to sign in screen', () => {
+    describe('when not authenticated', () => {
+      it('Should redirect to sign in screen', () => {
         const fakeBookId = faker.datatype.uuid()
         cy.visit(`/book/${fakeBookId}/notes`, { failOnStatusCode: false })
 
         cy.url().should('include', '/login')
+      })
     })
-  })
+
+    describe('when authenticated', () => {
+      beforeEach(() => {
+        cy.setCookie('bookue-user', 'any_token')
+      })
+
+      it('Should change to delete mode when user click in delete mode icon', () => {
+        const notesCount = 6
+        const fakeBookId = faker.datatype.uuid()
+        cy.visit(`/book/${fakeBookId}/notes`)
+
+        cy.getByTestId('notes-delete-mode-button').click()
+
+        cy.getByTestId('notes-note-card-delete-mode').should('have.length', notesCount)
+      })
+    })
 })
