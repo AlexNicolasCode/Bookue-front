4bdea47014e0cc19336650c97d671fbf47918994
Alex Nicolas <alexoliveira604@gmail.com>
2023-09-10 19:27:02 -0300

    feat: ModeProvider was moved to app file to enable hook correct usage and avoid broken screen

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import { AlertProvider } from '@/presentation/contexts'
+import { AlertProvider, ModeProvider } from '@/presentation/contexts'
 
 import '@/presentation/styles/reset.styles.css'
 import '@/presentation/styles/global.styles.css'
@@ -8,7 +8,9 @@ import '@/presentation/styles/global.styles.css'
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <AlertProvider>
-            <Component {...pageProps} />
+            <ModeProvider>
+                <Component {...pageProps} />
+            </ModeProvider>
         </AlertProvider>
     )
 }
