48f04fbd023a8ba9c61ac3d60ea1408ed31f8fb1
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-31 13:08:24 -0300

    test: RemoteAddNote should call HttpClient with correct params

--- tests/data/usecases/remote.add.note.spec.ts
+++ tests/data/usecases/remote.add.note.spec.ts
@@ -1,27 +1,73 @@
+import { faker } from '@faker-js/faker';
+
 import { AddNote } from '@/domain/usecases';
 import { LoadCookie } from '../protocols/cookie';
+import { HttpClient } from '../protocols/http';
 
 import { CookieManagerAdapterSpy } from '@/tests/infra/mocks';
 import { mockAddNoteParams } from '@/tests/main/domain/mocks';
+import { HttpClientSpy } from '../mocks';
   
 export class RemoteAddNote implements AddNote {
     constructor(
         private readonly loadCookie: LoadCookie,
+        private readonly url: string,
+        private readonly httpClient: HttpClient,
     ) {}
   
     async add(params: AddNote.Params): Promise<void> {
-      await this.loadCookie.load('bookue-user')
+      const accessToken = await this.loadCookie.load('bookue-user')
+      await this.httpClient.request({
+        url: this.url,
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          query: `
+              mutation AddNote($bookId: String!, $text: String!) {
+                  addNote(bookId: $bookId, text: $text!) {}
+              }
+            `,
+  
+          variables: { 
+            bookId: params.bookId,
+            text: params.text,
+          },
+        })
+      })
     }
   }
 
 describe('RemoteAddNote', () => {
   test('should call CookieManagerAdapterSpy with correct key', async () => {
-    const cookieManagerAdapterSpy = new CookieManagerAdapterSpy()
-    const sut = new RemoteAddNote(cookieManagerAdapterSpy);
+    const cookieManagerAdapterSpy = new CookieManagerAdapterSpy();
+    const url = faker.internet.url();
+    const httpClientSpy = new HttpClientSpy();
+    const sut = new RemoteAddNote(cookieManagerAdapterSpy, url, httpClientSpy);
     const fakeRequest = mockAddNoteParams();
 
     await sut.add(fakeRequest);
 
     expect(cookieManagerAdapterSpy.key).toBe('bookue-user');
+  });
+
+  test('should call HttpClient with correct params', async () => {
+    const cookieManagerAdapterSpy = new CookieManagerAdapterSpy();
+    const url = faker.internet.url();
+    const httpClientSpy = new HttpClientSpy();
+    const sut = new RemoteAddNote(cookieManagerAdapterSpy, url, httpClientSpy);
+    const fakeRequest = mockAddNoteParams();
+    const accessToken = cookieManagerAdapterSpy.result;
+
+    await sut.add(fakeRequest);
+
+    expect(httpClientSpy.url).toBe(url)
+    expect(httpClientSpy.method).toBe('post')
+    expect(httpClientSpy.headers).toStrictEqual({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+    })
   });
 });
