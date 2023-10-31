154fff9169cd336a005c4538cceaf093a222f3b5
Alex Nicolas <alexoliveira604@gmail.com>
2023-10-31 12:45:38 -0300

    refactor: apply note context to improve code redability

--- pages/book/[id]/notes/index.tsx
+++ pages/book/[id]/notes/index.tsx
@@ -1,11 +1,7 @@
 import { GetServerSideProps } from "next";
-import { useEffect, useState } from "react";
-import { faker } from "@faker-js/faker";
-
 
 import {
     Header,
-    Input,
     MainContent,
     NoteList,
     Option,
@@ -16,68 +12,28 @@ import { NoteModel } from "@/domain/models";
 import { useModeController } from "@/presentation/hook";
 import { Modes } from "@/presentation/contexts";
 import { makeRemoteLoadNotes } from "@/main/factory/usecases";
+import { NoteProvider } from "@/presentation/contexts/note";
 
 type NotesPageProps = {
     notes: NoteModel[]
 }
 
 export default function NotesPage({ notes }: NotesPageProps) {
-    const [listedNotes, setListedNotes] = useState<NoteModel[]>(notes)
-    const [newNote, setNewNote] = useState<string>('')
-    const { mode, lastMode } = useModeController()
-
-    useEffect(() => {
-        if (shouldAddNoteInList) {
-            addNoteInList()
-        }
-    }, [mode])
-
-    const shouldAddNoteInList =
-        newNote
-        && newNote.trim() !== ''
-        && lastMode.current === Modes.AddMode
-        && mode === Modes.DefaultMode
-    
-    const addNoteInList = () => {
-        setListedNotes([
-            {
-                id: faker.datatype.uuid(),
-                text: newNote,
-            },
-            ...listedNotes,
-        ])
-        setNewNote('')
-    }
-    
-    const deleteNote = (noteId: string) => {
-        const updatedNotes = listedNotes.filter((note) => note.id !== noteId)
-        setListedNotes(updatedNotes)
-    }
-
+    const { mode } = useModeController()
     return (
         <>
             <Header/>
             <MainContent>
-                <NoteList notes={listedNotes} deleteNote={deleteNote} />
-                {mode === Modes.AddMode &&
-                    <AddNoteContainer>
-                        <Input
-                            setState={setNewNote}
-                            value={newNote}
-                            style={{
-                                isBorded: true,
-                                height: '100%',
-                                width: '100%',
-                            }}
-                        />
-                    </AddNoteContainer>
-                }
-                <Options
-                    options={[Option.DeleteNote, Option.AddNote]}
-                    config={{
-                        hasBackground: true,
-                    }}
-                />
+                <NoteProvider>
+                    <NoteList ssrNotes={notes} />
+                    {mode === Modes.AddMode && <AddNoteContainer />}
+                    <Options
+                        options={[Option.DeleteNote, Option.AddNote]}
+                        config={{
+                            hasBackground: true,
+                        }}
+                    />
+                </NoteProvider>
             </MainContent>
         </>
     )
