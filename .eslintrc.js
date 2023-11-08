99c7fe912b879f4a5ecebf4beed2f39103a9e0d4
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-08 18:24:33 -0300

    chore: update eslint config to enable use with typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,16 @@ module.exports = {
     node: true,
   },
 
+  parser: "@typescript-eslint/parser",
+
   parserOptions: {
     ecmaVersion: 2020,
+    sourceType: "module",
   },
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-namespace': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-explicit-any': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
 
   overrides: [
