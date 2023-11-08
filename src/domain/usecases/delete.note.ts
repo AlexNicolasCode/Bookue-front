44854b1c890e9cd50f75cdcf75a8b9716ee02a89
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-08 15:38:23 -0300

    feat: DeleteNote usecase was created

---
+++ src/domain/usecases/delete.note.ts
@@ -0,0 +1,10 @@
+export interface DeleteNote {
+    delete: (params: DeleteNote.Params) => Promise<void>
+}
+
+export namespace DeleteNote {
+    export type Params = {
+        bookId: string
+        noteId: string
+    }
+}
