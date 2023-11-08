0a926eeac3e75801c5b048bb1b8b67d710207920
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-08 15:51:07 -0300

    test: RemoteDeleteNote should call HttpClient with correct params

--- tests/data/usecases/remote.delete.note.spec.ts
+++ tests/data/usecases/remote.delete.note.spec.ts
@@ -1,16 +1,42 @@
+import { faker } from '@faker-js/faker';
+
 import { DeleteNote } from '@/domain/usecases';
+import { HttpClient } from '@/data/protocols/http';
 import { LoadCookie } from '@/data/protocols/cookie';
 
 import { LoadCookieSpy } from '@/tests/infra/mocks';
 import { mockDeleteNoteParams } from '@/tests/main/domain/mocks';
+import { HttpClientSpy } from '../mocks';
 
 class RemoteDeleteNote implements DeleteNote {
   constructor(
         private readonly loadCookie: LoadCookie,
+        private readonly url: string,
+        private readonly httpClient: HttpClient,
   ) {}
 
   async delete(params: DeleteNote.Params): Promise<void> {
-    await this.loadCookie.load('bookue-user')
+    const accessToken = await this.loadCookie.load('bookue-user')
+    await this.httpClient.request({
+      url: this.url,
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        query: `
+            mutation DeleteNote($bookId: String!, $noteId: String!) {
+              deleteNote(bookId: $bookId, noteId: $noteId)
+            }
+          `,
+
+        variables: { 
+          bookId: params.bookId,
+          noteId: params.noteId,
+        },
+      })
+    })
   }
 }
 
@@ -18,11 +44,31 @@ class RemoteDeleteNote implements DeleteNote {
 describe('RemoteDeleteNote', () => {
   test('should call LoadCookie with correct key', async () => {
     const loadCookieSpy = new LoadCookieSpy();
-    const sut = new RemoteDeleteNote(loadCookieSpy);
+    const url = faker.internet.url();
+    const httpClientSpy = new HttpClientSpy();
+    const sut = new RemoteDeleteNote(loadCookieSpy, url, httpClientSpy);
     const fakeRequest = mockDeleteNoteParams();
 
     await sut.delete(fakeRequest);
 
     expect(loadCookieSpy.key).toBe('bookue-user');
+  });
+
+  test('should call HttpClient with correct params', async () => {
+    const loadCookieSpy = new LoadCookieSpy();
+    const url = faker.internet.url();
+    const httpClientSpy = new HttpClientSpy();
+    const sut = new RemoteDeleteNote(loadCookieSpy, url, httpClientSpy);
+    const fakeRequest = mockDeleteNoteParams();
+    const accessToken = loadCookieSpy.result;
+
+    await sut.delete(fakeRequest);
+
+    expect(httpClientSpy.url).toBe(url)
+    expect(httpClientSpy.method).toBe('post')
+    expect(httpClientSpy.headers).toStrictEqual({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+    })
   });
 });
