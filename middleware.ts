d021c36dc1abd499c97150f210577d0ab777164c
Alex Nicolas <alexoliveira604@gmail.com>
2023-11-13 13:11:04 -0300

    feat: set basic feature flag system to block user access selected screen

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
+import env from '@/main/config/env'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  if (shouldRedirectToNotFoundScreen(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL('/404', request.url))
+  }
   const hasAccessToken = request.cookies.has('bookue-user')
   const isSignInScreen = request.nextUrl.pathname.startsWith('/login')
   const isSignUpScreen = request.nextUrl.pathname.startsWith('/sign-up')
@@ -16,4 +20,15 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: ['/', '/book/:path*', '/login', '/sign-up'],
+}
+
+const shouldRedirectToNotFoundScreen = (pathname: string): boolean => {
+  const paths = [
+    { name: '/', isEnable: env.SCREEN.HOME },
+    { name: '/notes', isEnable: env.SCREEN.NOTES },
+    { name: '/add', isEnable: env.SCREEN.ADD_BOOK },
+    { name: '/login', isEnable: env.SCREEN.SIGN_IN },
+    { name: '/sign-up', isEnable: env.SCREEN.SIGN_UP },
+  ]
+  return paths.some((path) => pathname.endsWith(path.name) && !path.isEnable)
 }
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,11 @@
 export default {
   BOOKUE_API_URL: process.env.NEXT_PUBLIC_BOOKUE_API_URL ?? 'http://localhost:8000/graphql/',
+  SCREEN: {
+    DETAILS: process.env.NEXT_PUBLIC_DETAILS_SCREEN === 'enable' ? true : false,
+    NOTES: process.env.NEXT_PUBLIC_NOTES_SCREEN === 'enable' ? true : false,
+    ADD_BOOK: process.env.NEXT_PUBLIC_ADD_BOOK_SCREEN === 'enable' ? true : false,
+    HOME: process.env.NEXT_PUBLIC_HOME_SCREEN === 'enable' ? true : false,
+    SIGN_UP: process.env.NEXT_PUBLIC_SIGN_UP_SCREEN === 'enable' ? true : false,
+    SIGN_IN: process.env.NEXT_PUBLIC_SIGN_IN_SCREEN === 'enable' ? true : false,
+  }
 }
